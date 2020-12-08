443c8df27f3e40a4a177f68ef177f30da1e27d1b
Kartasheva Ekaterina <38951185+radishka@users.noreply.github.com>
2020-12-08 17:16:49 +0300

    Update MeetupUtils.java

--- src/test/java/ru/ifmo/se/lab3/MeetupUtils.java
+++ src/test/java/ru/ifmo/se/lab3/MeetupUtils.java
@@ -10,9 +10,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class MeetupUtils {
     public static void getURLWithCookie(String url, WebDriver driver){
         driver.get(url);
-        driver.manage().addCookie(new Cookie.Builder("MEETUP_LANGUAGE", "language=ru&country=RU").build());
-        driver.manage().addCookie(new Cookie.Builder("MEETUP_MEMBER", "id=321185883&status=1&timestamp=1607283944&bs=0&tz=Europe%2FMoscow&zip=meetup2&country=ru&city=St.+Petersburg&state=&lat=59.93&lon=30.32&ql=false&s=c2efd4c48407d85ca76251f51bbb480f0aa24443&scope=ALL").build());
-        driver.manage().addCookie(new Cookie.Builder("MEETUP_BROWSER_ID", "\"id=f9a8a91e-b49b-46e3-aa29-5d970f2e80f2\"").build());
+        driver.manage().addCookie(new Cookie.Builder("MEETUP_LANGUAGE", "***").build());
+        driver.manage().addCookie(new Cookie.Builder("MEETUP_MEMBER", "***").build());
+        driver.manage().addCookie(new Cookie.Builder("MEETUP_BROWSER_ID", "***").build());
     }
 
     public static void fillSearchForm(String place, WebDriver driver, WebDriverWait wait) {
